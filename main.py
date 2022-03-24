de514f4e2d9f0e8ad8f046b1fec386f8f9f4f6c8
Adham Salama <adhamsalama@zohomail.com>
2022-03-25 01:52:03 +0200

    Add error handling

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from trajectory import trajectory
 
 def main():
     kinematics = {1: forward, 2: inverse, 3: jacobian, 4: trajectory}
-    _input = int(input(
-        ' Choose Kinematics type:\n\
-        1 = Forward\n\
-        2 = Inverse\n\
-        3 = Jacobian\n\
-        4 = Trajectory\n'))
-    kinematics[_input]()
+    try:
+        while True:
+            kinematic = int(input(
+                'Choose Kinematics type:\n\
+            1 = Forward\n\
+            2 = Inverse\n\
+            3 = Jacobian\n\
+            4 = Trajectory\n'))
+            if 1 <= kinematic <= 4:
+                break
+        kinematics[kinematic]()
+    except:
+        print('Error')
 
 
 if __name__ == '__main__':
