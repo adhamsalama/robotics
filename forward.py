41910c8efc95e2ae2a34399e6dc467376e0f71c7
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2022-03-25 18:29:57 +0200

    Update forward.py

--- forward.py
+++ forward.py
@@ -7,11 +7,41 @@ def forward(*args, **kwargs):
     matrices = []
     for i in range(number_of_joints):
         a, alpha, d, theta = [float(i) for i in input(
-            f'Enter a, alpha, d, theta for joint {i+1}: ').split(' ')]
+            f'Enter a, alpha, d, theta for joint {i + 1}: ').split(' ')]
         matrices.append(A_matrix(a, alpha, d, theta))
     A = multiply_matrices([a.matrix for a in matrices])
-    print(np.round(A, decimals=3))
+    A = np.round(A, decimals=3)
+    print("\nT = \n", A)
+    end_effector_position(A)
 
+ 
+def end_effector_position(T):
+    n = len(T) - 1
+    p = []
+    for i in range(n):
+        p.append(T[i][n])
+    print("\nXe Ye Ze :", p)
+
+    col = []
+    row = []
+    for i in range(n):
+        col.append(T[i][0])
+    for i in range(n):
+        row.append(T[n - 1][i])
+
+    tan_phi = col[1] / col[0] if col[0] else 0
+    tan_theta = row[0] / math.sqrt(1-math.pow(row[0], 2)) if row[0] else 0
+    tan_epsi = row[1] / row[2] if row[2] else 0
+
+    phi = math.degrees(math.atan(tan_phi))
+    theta = math.degrees(math.atan(tan_theta))
+    epsi = math.degrees(math.atan(tan_epsi))
+
+    print("\nΦ =", np.round(phi, 3))
+    print("Θ =", np.round(theta, 3))
+    print("Ψ =", np.round(epsi, 3))
+    
+    
 
 def multiply_matrices(matrices):
     result = matrices[0]
