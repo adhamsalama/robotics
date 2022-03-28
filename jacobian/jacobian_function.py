5a972a15c48cac27f4e8a12d94e341c5e6db0746
mohamed-el-sharkawy <=>
2022-03-28 19:26:15 +0200

    inverse jacobian

--- jacobian/jacobian_function.py
+++ jacobian/jacobian_function.py
@@ -1,41 +1,41 @@
 from forward.A_matrix import A_matrix
 import numpy as np
 
-
-def jacobian(*args, **kwargs):
-    number_of_joints = int(input('Number of joints: '))
+def jacobian():
+    robotArrangement = input('Enter the robot arrangement (eg. RRP): ')
     matrices = []
-    types = []
-    for i in range(number_of_joints):
-        t = input(
-            f'Enter the type of the joint number {i+1} (R for revolute P for prismatic): ')
-        types.append(t)
+    
+    for i in range(len(robotArrangement)):
         a, alpha, d, theta = [float(i) for i in input(
             f'Enter a, alpha, d, theta for joint {i+1}: ').split(' ')]
         matrices.append(A_matrix(a, alpha, d, theta))
+    jac = calc_jacobian(robotArrangement , matrices)
+    for i in range(6):
+        for j in range(len(robotArrangement)):
+            print(jac[i][j], end='\t')
+        print()
+
+def calc_jacobian(robotArrangement, matrices):
     postionVector = genPositionVector([a.matrix for a in matrices])
     zVector = genZVector([a.matrix for a in matrices])
     jac = []
     for i in range(6):
         jac.append([])
-    n = len(types)
+    n = len(robotArrangement)
     for i in range(n):
         jv = []
-        if types[i] == 'R':
+        if robotArrangement[i] == 'R' or robotArrangement[i] == 'r':
             jv = np.cross(zVector[i], (postionVector[n]-postionVector[i]))
             for j in range(3):
                 jac[j].append(round(jv[j], 3))
             for j in range(3, 6):
                 jac[j].append(round(zVector[i][j-3], 3))
-        if types[i] == 'P':
+        if robotArrangement[i] == 'P' or robotArrangement[i] == 'p':
             for j in range(3):
                 jac[j].append(round(zVector[i][j], 3))
             for j in range(3, 6):
                 jac[j].append(0)
-    for i in range(6):
-        for j in range(n):
-            print(jac[i][j], end='\t')
-        print()
+    return jac
 
 
 def multiply_matrices(matrices):
@@ -66,3 +66,4 @@ def genPositionVector(matrices):
         result = np.dot(result, matrices[i])
         O.append(np.array([result[0, 3], result[1, 3], result[2, 3]]))
     return O
+
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from forward.forward_kinematics import forward_kinematics
 from inverse.inverse_kinematics import inverse_kinematics
 from jacobian.jacobian_function import jacobian
+from inverse_jacobian.inverse_jacobian_function import inverse_jacobian
 from trajectory.trajectory_solver import trajectory
-
 
 def main():
     kinematics = {1: forward_kinematics,
-                  2: inverse_kinematics, 3: jacobian, 4: trajectory}
+                  2: inverse_kinematics, 3: jacobian, 4: inverse_jacobian, 5: trajectory}
     try:
         while True:
             kinematic = int(input(
@@ -14,8 +14,9 @@ def main():
             1 = Forward\n\
             2 = Inverse\n\
             3 = Jacobian\n\
-            4 = Trajectory\n'))
-            if 1 <= kinematic <= 4:
+            4 = Inverse_jacobian\n\
+            5 = Trajectory\n'))
+            if 1 <= kinematic <= 5:
                 break
         kinematics[kinematic]()
     except Exception as e:
